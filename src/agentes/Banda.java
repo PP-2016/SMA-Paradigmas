a63ced1292d550acd5a6a254de78a8c840be24be
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-06-10 09:07:58 -0300

    Changing Banda Agent

--- src/agentes/Banda.java
+++ src/agentes/Banda.java
@@ -1,6 +1,11 @@
 package agentes;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import comportamentos.ComportamentoBanda;
 import jade.core.AID;
 import jade.lang.acl.MessageTemplate;
@@ -22,33 +27,27 @@ public class Banda extends Agent {
 	@Override
 	protected void setup(){
 		System.out.println("Banda "+getAID().getLocalName()+" no palco!");
-		addBehaviour(new CyclicBehaviour(this) {
-			
+		addBehaviour(new TickerBehaviour(this, 2000) {
+
 			@Override
-			public void action() {
-				ACLMessage msgJuiz = receive(MessageTemplate.MatchPerformative(ACLMessage.QUERY_REF));
-				if(msgJuiz != null)
-					addBehaviour(new ComportamentoBanda(myAgent,msgJuiz));
-				block();
-//				if(countB == 0){
-//					/*Criar classes que conversem com Banda para que 
-//					 * "EnviarMensagem" passe a funcinonar
-//					 */
-//					//enviaMensagem("Boa noite, galera! Pink floyd na �rea");
-//					System.out.println("Boa noite, galera! PInk floyd na �rea!!");
-//					
-//					
-//					
-//				 System.out.println(countB);
-//				}else if(msgAgente != null) {
-//					enviaMensagem("Boa noite!!");
-//					System.out.println(msgAgente.getContent());
-//				}else{
-//					this.block();
-//				}
+			protected void onTick() {
+					System.out.println("Cantando..");
+					DFAgentDescription template = new DFAgentDescription();
+					ServiceDescription sd = new ServiceDescription();
+					
+					sd.setType("Cantar-Musica");
+					template.addServices(sd);
+					try {
+						DFAgentDescription[] result = DFService.search(myAgent, template);
+					} catch (FIPAException e) {
+						e.printStackTrace();
+					}
+					
+					
 				
 			}
 		});
+		
 	}
 	
 	protected void takeDown(){
